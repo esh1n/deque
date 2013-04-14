0307408cf4a0abc95575ab6d85b2c17401981952
SunShelbi <android92>
2013-04-15 00:49:20 +0400

    fix test

--- deque_/main.cpp
+++ deque_/main.cpp
@@ -10,34 +10,11 @@ void printDeque(deque<std::wstring> deq)
    }
    std::tcout << (*itr).c_str() << "\n\n";
 }
-void printDeque(deque<int> deq)
-{
-	std::tcout<< "\n";
-   deque<int>::iterator itr(deq.begin());
-   while (itr != deq.end()) {
-	   std::tcout << (*itr) << "\n";
-        ++itr;
-   }
-   std::tcout << (*itr) << "\n\n";
-}
+
 int _tmain(int argc, _TCHAR* argv[])
 {
-	deque<int> deq_3;
-	deq_3.assign(3,3);
-	printDeque(deq_3);
-	int ar[5]={1,2,3,4,5};
-	deque<int> deq_4(ar,5);
-	deq_4.reverse();
-	printDeque(deq_4);
-	deque<std::wstring> deq_1;
-
-   deq_1.push_back(_T("and"));  
-   deq_1.push_front(_T("Love")); 
-   deq_1.push_back(_T("Peace"));    
-
+   deque<std::wstring> deq_1(5,_T("sample"));
    deque<std::wstring> deq_2(deq_1);
-   
-   
    bool loop=false;
    int index=0;
    std::tcout<<"\0-exit\n";
@@ -46,6 +23,11 @@ int _tmain(int argc, _TCHAR* argv[])
    std::tcout<<"3-pop_back\n";
    std::tcout<<"4-pop_front\n";
    std::tcout<<"5-[index] or at\n";
+   std::tcout<<"6-reverse\n";
+   std::tcout<<"7-back\n";
+   std::tcout<<"8-front\n";
+   std::tcout<<"9-size\n";
+   std::tcout<<"10-clear\n";
   
 	/////process/////////////////
 	do
@@ -85,13 +67,29 @@ int _tmain(int argc, _TCHAR* argv[])
 		   case '5':
 			   index=_tstoi(arg.c_str());
 			   try
-			    {
-			   std::tcout << (deq_2[index]).c_str() << "\n";
+			   {
+					std::tcout <<"\n"<< (deq_2[index]).c_str() << "\n";
 			   }
 			   catch(std::invalid_argument ex)
-			    {
-					 std::tcout<<ex.what();
+			   {
+					std::tcout<<ex.what();
 			   }
+			   break;
+		   case '6':
+			   deq_2.reverse();
+			   break;
+          case '7':
+			   std::tcout <<"\n"<< (deq_2.back()).c_str() << "\n";
+			   break;
+		  case '8':
+			   std::tcout <<"\n"<< (deq_2.front()).c_str() << "\n";
+			   break;
+		  case '9':
+			  std::tcout <<"\n"<< deq_2.size() << "\n";
+			   break;
+		  case '10':
+				   deq_2.clear() ;
+				   std::tcout <<"\n\n";
 			   break;
 		   default:
 			   break;
